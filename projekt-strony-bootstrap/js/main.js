2f5ede093fb0cdb1109260174021db161c247bb7
Magda Paluch <m.magdalena.pal@gmail.com>
2018-07-08 14:58:37 +0200

    contact us + footer

---
+++ projekt-strony-bootstrap/js/main.js
@@ -0,0 +1,26 @@
+//'use strict';
+//
+//const links =$("a:not([href='#'])");
+//const body = $("body");
+//const scrollTime = 600;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//function scrollWindow() {
+    'use strict';
+//    if ($(window).scrollTop() > 20) {
+//        $("html", "body").addClass("scroll-move")
+//    } else ()
+//}
+//
+//
+//
+//
+//$(window).on("scroll", scrollWindow);
